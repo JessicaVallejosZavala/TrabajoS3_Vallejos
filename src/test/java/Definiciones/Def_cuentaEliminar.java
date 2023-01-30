63096f4c6c3d9212d73421f26234eb3c63c1ef89
Jessica Vallejos <jessicavallejos@MacBook-Pro-de-Jessica.local>
2023-01-30 18:25:17 -0300

    segundo intento

--- src/test/java/Definiciones/Def_cuentaEliminar.java
+++ src/test/java/Definiciones/Def_cuentaEliminar.java
@@ -7,7 +7,7 @@ public class Def_cuentaEliminar {
 
     Cuenta cuenta;
 
-
+//Para escenario de prueba de eliminar cuenta
     @When("Selecciono opcion eliminar {string}")
     public void selecciono_opcion_eliminar(String string) throws InterruptedException {
         cuenta.opcionEliminar();
